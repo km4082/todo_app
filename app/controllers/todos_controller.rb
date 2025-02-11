287ae8e1af08682d55c795c0051dfa02ed5a0a23
km4082 <125015020+km4082@users.noreply.github.com>
2025-02-11 17:02:17 +0900

    todosコントローラー作成

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,22 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+  
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new
+    end
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :description)
+  end
+end
