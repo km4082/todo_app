94291178df74f047d964648f4b398901d1a505fc
km4082 <125015020+km4082@users.noreply.github.com>
2025-02-11 19:55:35 +0900

    ルーティング省略

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root "todos#index"
-  get "/todos", to: "todos#index"
-  get "/todos/new", to: "todos#new"
-  post "/todos", to: "todos#create"
-  get "/todos/:id", to: "todos#show"
 
   resources :todos
 end
